90ae52d88e99996afbd0d906d34243ea6a713fb8
Ivan Kalinić <ivankalinic1998@gmail.com>
2021-11-09 11:25:11 +0100

    Finished styling and layout of cards and columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./App.css";
+import { Column } from "./Column";
+import { Card } from "./Card";
+import { AppContainer } from "./styles";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <AppContainer>
+      <Column text="To Do">
+        <Card text="Generate app scaffold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn Typescript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing" />
+      </Column>
+    </AppContainer>
+  );
 }
 
 export default App;
