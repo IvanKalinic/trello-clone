6832250810d8842a50405520db80a4dfb5e10069
Ivan Kalinić <ivankalinic1998@gmail.com>
2021-11-10 15:09:33 +0100

    Finished drag and drop logic

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { AppStateProvider } from "./AppStateContext";
 import { DndProvider } from "react-dnd";
-import { HTML5Backend as Backend } from "react-dnd-html5-backend";
+import Backend from "react-dnd-html5-backend";
 
 ReactDOM.render(
   <DndProvider backend={Backend}>
