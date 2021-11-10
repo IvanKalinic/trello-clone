d5b6b8b843895e3a539171715af46a044e60e6ef
Ivan Kalinić <ivankalinic1998@gmail.com>
2021-11-10 12:25:33 +0100

    Enabled dispatch for add list and add task actions and started drag and drop

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,19 +1,31 @@
 import React from "react";
+import { AddNewItem } from "./AddNewItem";
+import { useAppState } from "./AppStateContext";
+import { Card } from "./Card";
 import { ColumnContainer, ColumnTitle } from "./styles";
 
 interface ColumnProps {
   text: string;
+  index: number;
+  id: string;
   //children?: React.ReactNode
 }
 
-export const Column = ({
-  text,
-  children,
-}: React.PropsWithChildren<ColumnProps>) => {
+export const Column = ({ text, index, id }: ColumnProps) => {
+  const { state, dispatch } = useAppState();
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
-      {children}
+      {state.lists[index].tasks.map((task) => (
+        <Card text={task.text} key={task.id} />
+      ))}
+      <AddNewItem
+        toggleButtonText="+ Add another task"
+        onAdd={(text) =>
+          dispatch({ type: "ADD_TASK", payload: { text, taskId: id } })
+        }
+        dark
+      />
     </ColumnContainer>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { AppStateProvider } from "./AppStateContext";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend as Backend } from "react-dnd-html5-backend";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <DndProvider backend={Backend}>
+    <AppStateProvider>
+      <App />
+    </AppStateProvider>
+  </DndProvider>,
+  document.getElementById("root")
+);
